bad622eb828a26a6d0e2b695764635cd797a91ab
AndrewMacgill1 <andrew.macgill@qa.com>
2018-03-27 17:10:26 +0100

    Testing JSON stuff

---
+++ account-project/src/main/java/hhhhh.java
@@ -0,0 +1,13 @@
+package tester;
+
+public class hhhhh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Service service2 = new Service();
+		Account aliceSmith = new Account("Alice","Smith",1234);
+		service2.addAcc(aliceSmith);
+		System.out.println(service2.Conversion());
+	}
+
+}
